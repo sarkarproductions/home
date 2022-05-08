022d249c632117cd6f5a2471e95c82a0a82e3324
techiemanish <manishtiwari627@gmail.com>
2022-05-08 16:14:32 +0530

    Navbar link bug fix and contact button added

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -11,7 +11,10 @@ function LandingPage(props) {
     <header className="w3-container w3-center" style={{padding: "128px 16px"}}>
     <h1 className="w3-margin w3-jumbo w3-animate-top" onClick={animation} style={{fontFamily:"Montserrat"}}>{props.title}</h1>
     <p className="w3-xlarge w3-animate-left">{props.slogan}</p>
-    <button className="w3-button w3-black w3-padding-large w3-large w3-margin-top"><Link to="/services" className='text-light'>Get Started</Link></button>
+    <div className="container">
+    <button className="w3-button w3-black w3-padding-large w3-large w3-margin-top mx-2"><Link to="/services" className='text-light'>Get Started</Link></button>
+    <button className="w3-button w3-black w3-padding-large w3-large w3-margin-top"><Link to="/contact" className='text-light'>Contact Us</Link></button>
+    </div>
     </header>
     </>
   )
